e2c490f477d88baaa77cce424d9b81771a35a282
MauComas <111325433+MauComas@users.noreply.github.com>
2026-03-06 20:17:40 -0600

    Nivelacos

--- BrickSouls/Assets/Scripts/Brick.cs
+++ BrickSouls/Assets/Scripts/Brick.cs
@@ -16,6 +16,7 @@ public class Brick : MonoBehaviour
             }
             gameObject.SetActive(false);
             //GameManager.instance.BlockDestroy();
+            Destroy(this.gameObject);
         }
     }
     // Start is called before the first frame update
